ebe18f17e5f9ae41e02228777c23acd6ff4b94b5
Angus Griffith <16sn6uv@gmail.com>
2015-08-04 22:29:34 +1000

    update index and link to local mathics server

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+// allow port links
+document.addEventListener('click', function(event) {
+  var target = event.target;
+  if (target.tagName.toLowerCase() == 'a')
+  {
+      var port = target.getAttribute('href').match(/^:(\d+)(.*)/);
+      if (port)
+      {
+         target.href = port[2];
+         target.port = port[1];
+      }
+  }
+}, false);
